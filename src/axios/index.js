a1e5ae6a8325da150ae515a1a48d60470618ea97
huangyuhui123 <2985380611@qq.com>
2020-02-13 14:45:14 +0800

    单选多选

--- src/axios/index.js
+++ src/axios/index.js
@@ -20,6 +20,11 @@ export default class Axios {
     }
 
     static ajax(options) {
+        let loading;
+        if(options.data && options.data.isShowloading !==false){
+            loading = document.getElementById('ajaxLoading');
+            loading.style.display = 'block'
+        }
         const baseurl = "http://yapi.demo.qunar.com/mock/80590/api/"
         return new Promise((resolve, reject) => {
             axios({
@@ -31,6 +36,10 @@ export default class Axios {
                 //headers：
                 //跨域的话需要传cookie
             }).then((response) => {
+                if(options.data && options.data.isShowloading !==false){
+                    loading = document.getElementById('ajaxLoading');
+                    loading.style.display = 'none'
+                }
                 if (response.status === 200 ) { //服务起返回的状态码
                     const res = response.data
                     if (res.code === '0') {// 业务层面定义的code
--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import{Card,Table} from 'antd';
+import{Card,Table, Modal, Button, message} from 'antd';
 import axios from './../../axios/index'
 
 export default class basicTable extends Component {
@@ -16,10 +16,15 @@ export default class basicTable extends Component {
                 state:"1"
             }
         ]
-        this.request()
+        data.map((item,index)=>{
+            item.key= index;
+        })
+        
         this.setState({
             dataSource:data
         })
+
+        this.request()
     }
     request =()=>{
      /*    const baseurl = "http://yapi.demo.qunar.com/mock/80590/api/"
@@ -45,13 +50,48 @@ export default class basicTable extends Component {
             }
         }).then((res)=>{
             if(res.code === "0"){
+                res.result.list.map((item,index)=>{
+                    item.key = index;
+                })
                 this.setState({
-                    dataSource2: res.result.list
+                    dataSource2: res.result.list,
+                    selectedRowKeys:[],
+                    selectedRows:[]
                 })
             }
         })
     }
 
+    onRowClick = (record,index) =>{
+        console.log('dddd')
+        let selectKey = [index]; //多选就是个数组
+        Modal.info({
+            title:'信息',
+            content:`用户名：${record.userName}`
+        })
+        this.setState({
+            selectedRowKeys:selectKey,
+            selectedItem:record
+        })
+
+    }
+    //多选执行删除操作
+    handleDelete=(()=>{
+        let rows = this.state.selectedRows;
+        let ids = [];
+        rows.map((item)=>{
+            ids.push(item.id)
+        })
+        Modal.confirm({
+            title:"删除提示",
+            content:`您确定要删除这些数据吗？${ids.join(',')}`,
+            onOk:()=>{
+                message.success('删除成功')
+                this.request()
+            }
+        })
+
+    })
     render() {
         const columns=[
             {
@@ -64,15 +104,41 @@ export default class basicTable extends Component {
             },
             {
                 title:"性别",
-                dataIndex:"sex"
+                dataIndex:"sex",
+                render(sex){
+                    return sex ===1 ? '男':'女'
+                }
             },
             {
                 title:"状态",
-                dataIndex:"state"
+                dataIndex:"state",
+                render(state){
+                    let config = {
+                        "1":'咸鱼一条',
+                        "2":"风华浪子",
+                        "3":"北大才子",
+                        "4":"百度FE",
+                        "5":"创业者"
+                    }
+                    return config[state]
+                }
             },
             {
                 title:"兴趣",
-                dataIndex:"interest"
+                dataIndex:"interest",
+                render(interest){ //interst是形参,可以写为任何变量
+                    let config = {
+                        "1":'游泳',
+                        "2":"打篮球",
+                        "3":"踢足球",
+                        "4":"跑步",
+                        "5":"爬山",
+                        "6":"骑行",
+                        "7":"桌球",
+                        "8":"麦霸"
+                    }
+                    return config[interest]
+                }
             },
             {
                 title:"生日",
@@ -87,6 +153,34 @@ export default class basicTable extends Component {
                 dataIndex:"time"
             }
         ]
+        const {selectedRowKeys} = this.state;
+        const rowSelection = {
+            type:'radio',
+            selectedRowKeys,
+            onChange:(selectedRowKeys,selectedRows)=>{
+                this.setState({
+                    selectedRowKeys,
+                })
+            }
+        }
+        const rowCheckSelection ={
+            type:'checkbox',
+            selectedRowKeys,
+            onChange:(selectedRowKeys,selectedRows)=>{
+                let ids = [];
+                console.log(selectedRows)
+                selectedRows.map((item)=>{
+                   ids.push(item.id); 
+
+                })
+                this.setState({
+                    selectedRowKeys,
+                    selectedRows,
+                    //selectedIds:ids
+                })
+            }
+        }
+    
         return (
             <div>
                 <Card title="基础表格">
@@ -102,8 +196,48 @@ export default class basicTable extends Component {
                         bordered
                         columns={columns}
                         dataSource={this.state.dataSource2}
-
                     ></Table>
+                </Card>
+                <Card title="Mock-单选" style={{margin:'10px 0'}}>
+                    <Table  
+                        bordered
+                        rowSelection ={rowSelection}
+                        onRow={(record,index) => {
+                            return {
+                              //onClick: event => {}, // 点击行
+                              onClick:()=>{
+                                  //方法定义在外面
+                                  this.onRowClick(record,index)
+                              }
+                            };
+                          }}
+                        columns={columns}
+                        dataSource={this.state.dataSource2}
+                       
+                    >
+                    </Table>
+                </Card>
+                <Card title="Mock-多选" style={{margin:'10px 0'}}>
+                    <div>
+                        <Button onClick={this.handleDelete}></Button>
+                    </div>
+                    <Table  
+                        bordered
+                        rowSelection ={rowCheckSelection}
+                        onRow={(record,index) => {
+                            return {
+                              //onClick: event => {}, // 点击行
+                              onClick:()=>{
+                                  //方法定义在外面
+                                  this.onRowClick(record,index)
+                              }
+                            };
+                          }}
+                        columns={columns}
+                        dataSource={this.state.dataSource2}
+                       
+                    >
+                    </Table>
                 </Card>
             </div>
         )
